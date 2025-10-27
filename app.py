e0f519d73babc3e9755c5320869326d0753b2dc0
Vihar Thakkar <n01634428@humber.ca>
2025-10-27 09:08:44 -0400

    final commit before deploying

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ app.secret_key = os.getenv('secret_key')
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] =  os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT'))
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 
